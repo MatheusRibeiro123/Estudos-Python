b2a15c2e3d859f54b7be300b8dd4597f2866a529
Matheus Ribeiro <mathribemathribe@gmail.com>
2026-03-06 12:21:08 -0300

    atualizaçoes estudos

---
+++ testeLogica10.py
@@ -0,0 +1,6 @@
+numero = int(input("Difite seu numero: "))
+
+if numero / 1 == 0 and numero / numero ==1:
+    print(f"{numero} é primo.")
+else:
+    print(f"{numero} nao é primo.")
---
+++ testeLogica9.py
@@ -0,0 +1,7 @@
+numero = int(input("Qual seu numero: "))
+soma_total=0
+
+for i in range(1,numero + 1):
+    soma_total = soma_total + i 
+    
+print(soma_total)
