c897e335cec02e1d9e2c0714985ca9acda02efc3
Matheus Ribeiro <mathribemathribe@gmail.com>
2026-03-07 14:55:39 -0300

    atualizaçoes dos estudos

--- testeLogica11.py
+++ testeLogica11.py
@@ -1,10 +1,11 @@
-numero1 = int(input("Digite seu primeiro numero:  "))
-numero2 = int(input("Digite seu segundo numero:  "))
-numero3 = int(input("Digite seu terceiro numero:  "))
+# numero1 = int(input("Digite seu primeiro numero:  "))
+# numero2 = int(input("Digite seu segundo numero:  "))
+# numero3 = int(input("Digite seu terceiro numero:  "))
 
-if numero1 > numero2 and numero1 > numero3:
-    print(numero1)
-elif numero2 > numero1 and numero2 > numero3:
-    print(numero2)
-else:
-    print(numero3)
+# if numero1 >= numero2 and numero1 >= numero3:
+#     print(numero1)
+# elif numero2 >= numero1 and numero2 >= numero3:
+#     print(numero2)
+# else:
+#     print(numero3)
+
